431bfff3cbef2450169d0362a41064d45f0023a9
Pavan Chindukuri <chindukuripavan@gmail.com>
2022-02-09 09:32:38 +0530

    adding ownerOf function

--- contracts/ERC721.sol
+++ contracts/ERC721.sol
@@ -7,10 +7,18 @@ contract ERC721 {
     // event ApprovalForAll
 
     mapping(address => uint256) internal _balances;
+    mapping(uint256 => address) _owners;
 
     // Returns the number of NFT's assigned to the owner
     function balanceOf(address _owner) public view returns (uint256) {
         require(_owner != address(0), "Can not be an empty address");
         return _balances[_owner];
+    }
+
+    // Finds the owner of the NFT
+    function ownerOf(uint256 _tokenId) public view returns (address) {
+        address owner = _owners[_tokenId];
+        require(owner != address(0), "Token ID does not exist");
+        return owner;
     }
 }
